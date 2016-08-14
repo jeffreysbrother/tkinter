ec0108782b851067733b392596a302d85d192c03
James Cool <jeffreysbrother@gmail.com>
2016-08-14 10:39:16 -0700

    not working, bad library?

---
+++ message.py
@@ -0,0 +1,8 @@
+from Tkinter import *
+import Tkinter.messagebox
+root = Tk()
+
+# ********* Message Box *********
+Tkinter.messagebox.showinfo('window title', 'Bananna Butt Equestrian')
+
+root.mainloop()
