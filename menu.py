347488ff026aba73876d90b0f1ac8db997fec195
James Cool <jeffreysbrother@gmail.com>
2016-08-13 19:05:57 -0700

    add status bar

--- menu.py
+++ menu.py
@@ -8,6 +8,7 @@ root = Tk()
 
 menu = Menu(root)
 root.config(menu=menu)
+root.geometry("400x600")
 
 subMenu = Menu(menu)
 menu.add_cascade(label="File", menu=subMenu)
@@ -19,5 +20,19 @@ subMenu.add_command(label="Exit", command=doNothing)
 editMenu = Menu(menu)
 menu.add_cascade(label="Edit", menu=editMenu)
 editMenu.add_command(label="Redo", command=doNothing)
+
+# ********* Toolbar *********
+toolbar = Frame(root, bg="blue")
+insertButt = Button(toolbar, text="Dingus", command=doNothing)
+insertButt.pack(side=LEFT, padx=2, pady=2)
+
+printButt = Button(toolbar, text="Pringal", command=doNothing)
+printButt.pack(side=LEFT, padx=2, pady=2)
+
+toolbar.pack(side=TOP, fill=X)
+
+# ********* Status Bar *********
+
+
 
 root.mainloop()
