ddb0da40d8794b814f7f95cccc911451a4adc0ea
James Cool <jeffreysbrother@gmail.com>
2016-08-14 21:29:24 -0700

    open in center of screen

--- one.py
+++ one.py
@@ -13,7 +13,21 @@ def middleClick(event):
     print("wheelclick jackass bitch")
 
 root.title("application")
-root.geometry("400x600")
+
+w = 800 # width for the Tk root
+h = 650 # height for the Tk root
+
+# get screen width and height
+ws = root.winfo_screenwidth() # width of the screen
+hs = root.winfo_screenheight() # height of the screen
+
+# calculate x and y coordinates for the Tk root window
+x = (ws/2) - (w/2)
+y = (hs/2) - (h/2)
+
+# set the dimensions of the screen 
+# and where it is placed
+root.geometry('%dx%d+%d+%d' % (w, h, x, y))
 
 #button
 button_1 = Button(root, text="click me", command=printName)
